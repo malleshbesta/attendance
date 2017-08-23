58427a02c04ef17d8e13bc07d7f986c7f9270c4c
Swaroop Sagar <swaroopsagar@lt-gst-0495.karvy.com>
2017-08-23 17:18:27 +0530

    added seed file with user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Warehouse.create(name: "runner1", latitude: "hhediie112", longtitude: "hhderfee3355", user_id: 1, time_in: "2017-08-24", time_out: "2017-08-24")
-Warehouse.create(name: "runner2", latitude: "hhediie155", longtitude: "hhderfee433", user_id: 2, time_in: "2017-08-23", time_out: "2017-08-23")
-Warehouse.create(name: "runner3", latitude: "hhediie1552", longtitude: "hhderfee77", user_id: 3, time_in: "2017-08-22", time_out: "2017-08-22")
-Warehouse.create(name: "runner4", latitude: "hhediie442", longtitude: "hhderfee366", user_id: 4, time_in: "2017-08-21", time_out: "2017-08-21")
-Warehouse.create(name: "runner5", latitude: "hhediie1166", longtitude: "hhderfee44", user_id: 5, time_in: "2017-08-20", time_out: "2017-08-20")
+@user = User.last
+
+Warehouse.create(name: "runner1", latitude: "hhediie112", longtitude: "hhderfee3355", user_id: @user.id, time_in: "2017-08-24", time_out: "2017-08-24")
+Warehouse.create(name: "runner2", latitude: "hhediie155", longtitude: "hhderfee433", user_id: @user.id, time_in: "2017-08-23", time_out: "2017-08-23")
+Warehouse.create(name: "runner3", latitude: "hhediie1552", longtitude: "hhderfee77", user_id: @user.id, time_in: "2017-08-22", time_out: "2017-08-22")
+Warehouse.create(name: "runner4", latitude: "hhediie442", longtitude: "hhderfee366", user_id: @user.id, time_in: "2017-08-21", time_out: "2017-08-21")
+Warehouse.create(name: "runner5", latitude: "hhediie1166", longtitude: "hhderfee44", user_id: @user.id, time_in: "2017-08-20", time_out: "2017-08-20")
